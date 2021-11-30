51a66ad27ff17399e87ce1f651990b5edf274f20
Keimal <93018836+KeiRey@users.noreply.github.com>
2021-11-30 08:40:48 +0700

    Add files via upload

---
+++ uas_pbo_keimalreyyan_xiia/src/uas_pbo_keimalreyyan_xiia/Uas_pbo_keimalreyyan_xiia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas_pbo_keimalreyyan_xiia;
+
+/**
+ *
+ * @author prog3
+ */
+public class Uas_pbo_keimalreyyan_xiia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
